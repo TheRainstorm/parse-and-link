bfb6a5dc060bbb5ae270a190496666eb1c1ae958
Fuyan Yuan <33221728+TheRainstorm@users.noreply.github.com>
2023-12-17 17:51:07 +0800

    ✨ feat: add monitor mode
    - monitor media src, run automatically
    - add a *.run file to trigger a run manually

--- run_config.py
+++ run_config.py
@@ -1,28 +1,84 @@
 import os
 import argparse
 import json
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
 from pal.pal import PaL
 
+class MyHandler(FileSystemEventHandler):
+    def __init__(self, options) -> None:
+        super().__init__()
+        self.options = options
+        self.pal = PaL()
+    
+    def run_pal(self, file_path):
+        ext = os.path.splitext(file_path)[1]
+        if ext in ['.mp4', '.mkv', '.avi', '.rmvb'] or ext=='.run': # trigger
+            self.pal.pal(options=self.options)
+        
+    def on_created(self, event):
+        print(f"File created: {event.src_path}")
+        self.run_pal(event.src_path)
+        
+    def on_deleted(self, event):
+        print(f"File deleted: {event.src_path}")
+        self.run_pal(event.src_path)
+
 parser = argparse.ArgumentParser()
-parser.add_argument('-f',
+parser.add_argument('-c',
                     '--config-file',
                     default='config/example.json',
                     required=True,
                     help='The config file path')
-
+parser.add_argument('-m',
+                    '--monitor',
+                    action='store_true',
+                    help='Monitor mode, run if file created or deleted')
 args = parser.parse_args()
-with open(args.config_file, 'r') as f:
-    config = json.load(f)
 
-pal = PaL()
+def read_config():
+    # read config file
+    with open(args.config_file, 'r') as f:
+        config = json.load(f)
+    default = {}
+    if 'default' in config:
+        default = config['default']
 
-default = {}
-if 'default' in config:
-    default = config['default']
+    options_dict = {}
+    for d in config['list']:
+        options = default.copy()
+        options.update(d)
+        options_dict[d['media_src']] = options
+    return options_dict
 
-for d in config['list']:
-    print(d['media_src'])
-    options = default.copy()
-    options.update(d)
-    pal.pal(options=options)
+options_dict = read_config()
+
+if args.monitor:
+    observers = []
+    for watched_dir,options in options_dict.items():
+        # add observer
+        print(f'watched_dir: {watched_dir}')
+        event_handler = MyHandler(options)
+        observer = Observer()
+        observer.schedule(event_handler, path=watched_dir, recursive=True)
+        observers.append(observer)
+
+    for observer in observers:
+        observer.start()
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print('KeyboardInterrupt')
+        for observer in observers:
+            observer.stop()
+
+    for observer in observers:
+        observer.join()
+else:
+    for watched_dir,options in options_dict.items():
+        pal = PaL()
+        pal.pal(options=options)
