57125acf2affb3f75f11f40c580371022cc9521e
Fuyan Yuan <33221728+TheRainstorm@users.noreply.github.com>
2024-05-15 21:03:42 +0800

    "update entrypoin.sh support run as root"

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-PUID=${PUID:-1000}
-PGID=${PGID:-1000}
+PUID=${PUID:-0}
+PGID=${PGID:-0}
+
+if [ "$PUID" -eq 0 ]; then
+    echo "run as root"
+    if [ -n "$JELLYFIN_URL" ] && [ -n "$JELLYFIN_API_KEY" ]; then
+        echo "Jellyfin URL is set to $JELLYFIN_URL"
+        python3 run_config.py -c /config.json -m -j "$JELLYFIN_URL" -k "$JELLYFIN_API_KEY"
+    else
+        python3 run_config.py -c /config.json -m
+    fi
+    exit 0
+fi
 
 if [ `id -u abc` -ne $PUID ]; then
     usermod -u $PUID abc
